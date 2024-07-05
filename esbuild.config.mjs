f3fbb8c4d969fc10e909c56e8742f19978ed9c06
hankei6km <gh-hankei6km@outlook.jp>
2024-07-05 12:03:29 +0000

    chore: Define package bundling behavior explicitly
    Explicitly define it to avoid being affected because the default of esbuild has
    changed in a short period.(#174)

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -4,6 +4,7 @@ await esbuild.build({
   entryPoints: ['src/extension/extension.ts'],
   outfile: 'out/extension.js',
   bundle: true,
+  packages: 'bundle',
   external: ['vscode'],
   sourcemap: true,
   platform: 'node',
